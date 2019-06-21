2ef2a14745e603b5d05ec06afe4e04a3e63355af
ISTLOJAV <ISTLOJAV@ISTLOJA06>
2019-06-21 09:44:38 -0500

    Herencia trabajo en clase

--- PracticaPOORamiroCapa/src/Herencia21Junio/Profesor.java
+++ PracticaPOORamiroCapa/src/Herencia21Junio/Profesor.java
@@ -9,7 +9,32 @@ package Herencia21Junio;
  *
  * @author ISTLOJAV
  */
-public class Profesor {
-    //Atributos
-    
+public class Profesor extends Persona {
+
+    //atributos
+    private float sueldo;
+
+    public float getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(float sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public void darnota() {
+        System.out.println(" Tu nota es " + 7);
+    }
+
+    public void test() {
+        saludar();
+        super.saludar();
+    }
+
+    public static void main(String[] args) {
+        Profesor objeto = new Profesor();
+        objeto.test();
+        objeto.darnota();
+    }
+
 }
