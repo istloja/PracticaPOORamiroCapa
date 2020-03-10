642f356d93fee42d9c983dadbeb361e642bf5974
Ramiro Capa <49032646+lil101@users.noreply.github.com>
2020-03-10 07:05:13 -0600

    Botton verificar

---
+++ PracticaPOORamiroCapa/ConexionBD/src/Main/Ejecutar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Vista.Menu;
+
+/**
+ *
+ * @author John Puglla
+ */
+public class Ejecutar {
+    
+    
+    public static void main(String[] args) {
+        new Menu();
+        
+    }
+    
+}
